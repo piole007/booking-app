2257dfb686f3107d26349a7314df05239810219b
viola <pumpure@yahoo.com>
2023-08-23 19:04:13 -0600

    api added in index.js

--- src/components/loginForm/LoginForm.jsx
+++ src/components/loginForm/LoginForm.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./LoginForm.css"
 import { Link } from 'react-router-dom'
 
 const LoginForm = ({questionText, btnText, routText, rout, title} ) => {
+
+    
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
         <div className='mb-64'>
             <h1 className='text-4xl text-center mb-4'>{title}</h1>
             <form className='max-w-md mx-auto'>
-                <input className='email' type="email" placeholder='your@email.com' />
-                <input className="password" type="password" placeholder='password' />
+
+                <input className='email' 
+                type="email" 
+                placeholder='your@email.com' 
+                value={email} 
+                onChange={ev=>setEmail(ev.target.value)} />
+
+                <input className="password" 
+                type="password" 
+                placeholder='password'
+                value={password}
+                onChange={ev=>setPassword(ev.target.value)} />
+                 
                 <button className='login'>{btnText}</button>
                 <div className='text-center py-2 text-gray-500'>{questionText} <Link className='underline text-black' to={rout}>
                    {routText} 
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import LoginForm from '../components/loginForm/LoginForm'
 
 const RegisterPage = () => {
+
   return (
     <div className='mt-4 grow flex items-center justify-around'>
         <LoginForm questionText={"Already a member?"} btnText={"Register"} routText="Login" rout={"/login"} title={"Register"}/>
