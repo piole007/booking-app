44ba8fc47ff060e153c995d7fec2739c24ab79c1
ZymanteL <zymantelelesiute@gmail.com>
2023-08-28 15:13:50 +0300

    some code refactoring, upload photo from pc functionality

--- api/index.js
+++ api/index.js
@@ -7,6 +7,8 @@ const UserModel = require("./models/User");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const imageDownloader = require("image-downloader");
+const multer = require("multer")
+const fs = require('fs')
 
 const app = new express();
 const bcryptSalt = bcrypt.genSaltSync(10);
@@ -14,6 +16,7 @@ const jwtSecret = "randomStringHere";
 
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname + '/uploads'))
 app.use(
   cors({
     credentials: true,
@@ -95,17 +98,32 @@ router.get("*", (req, res) => {
   res.send("You accessed the node server");
 });
 
-app.use(router);
-
 app.post("/upload-by-link", async (req, res) => {
   const { link } = req.body;
-  const newName = Date.now() + ".jpg";
+  const newName = "photo" + Date.now() + ".jpg";
   await imageDownloader.image({
     url: link,
-    dest: __dirname + "/uploads" + newName,
+    dest: __dirname + "/uploads/" + newName,
   });
-  res.json(__dirname + "/uploads" + newName);
+  res.json(newName);
 });
+
+const photosMiddleware = multer({ dest: 'uploads/' })
+app.post("/upload", photosMiddleware.array('photos', 100), (req, res) => {
+  const uploadedFiles = []
+  for (let i = 0; i < req.files.length; i++) {
+    const { path, originalname } = req.files[i];
+    console.log(path);
+    const parts = originalname.split('.')
+    const ext = parts[parts.length - 1]
+    const newPath = path + '.' + ext;
+    fs.renameSync(path, newPath)
+    uploadedFiles.push(newPath.replace('uploads\\','')) //this part can be different for you, it can be uploads/, don't know why it's like this for me
+  }
+  res.json(uploadedFiles)
+})
+
+app.use(router);
 
 app.listen(4000, function () {
   console.log("Server is up on http://localhost:4000");
